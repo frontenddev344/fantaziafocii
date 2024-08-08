41a5150e726f6951d237f4e3e8c9bbf0cf4eec1b
leetbadd288 <frontenddev344@gmail.com>
2024-08-08 10:35:55 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,30 @@
 var events =  document.querySelectorAll(".events")
 events.forEach(function(elem){
 console.log(elem)
-elem.addEventListener("mouseenter", function(){
-var img = elem.getAttribute("src")
+var box =  elem.querySelector(".box")
+ var aEL = box.querySelector("a")
+elem.addEventListener("mouseenter", function(mE){
+var img = aEL.getAttribute("src")
 console.log(img)
 });
 });
+
+
+// var events = document.querySelectorAll(".events");
+
+// events.forEach(function(elem) {
+//     console.log(elem);
+//     var box = elem.querySelector(".box");
+
+//     if (box) {
+//         console.log(box);
+//         var img = box.querySelector("img"); // Select the image within the box
+
+//         if (img) {
+//             elem.addEventListener("mouseenter", function(mE) {
+//                 var imgSrc = img.getAttribute("src");
+//                 console.log(imgSrc);
+//             });
+//         }
+//     }
+// });
